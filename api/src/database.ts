2b69e16487a642b5deaa0e938911f1ea583112f9
Darren Hickling <darrenhickling@hotmail.com>
2019-12-05 20:57:54 +0000

    🐳 Adding a Docker image for the API Using dotenv-defaults to override Mongo settings

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,12 +1,13 @@
 import { createConnection } from "typeorm";
 import { User } from "./entities";
+require("dotenv-defaults").config();
 
 export const connectToDatabase = async () => {
     const connection = await createConnection({
         database: "test",
         entities: [User],
-        host: "localhost",
-        port: 27017,
+        host: process.env.DB_HOST || "localhost",
+        port: +(process.env.DB_PORT || 27017),
         type: "mongodb",
         useUnifiedTopology: true
     });
